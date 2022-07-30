436cbde27a3d63e264fe3e9220b8137c47bdfda0
T.Shutta <tksst51@gmail.com>
2022-07-30 11:42:36 +0900

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by 習田武志 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
